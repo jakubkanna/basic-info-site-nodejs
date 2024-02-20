cfa9fedda1782df6967d74b35024d07c146a3454
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-02-20 11:35:40 +0100

    serves all files (switch)

--- index.js
+++ index.js
@@ -6,23 +6,32 @@ const hostname = "localhost";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        return res.end(`Error loading index.html: ${err}`);
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-
-      res.end(data); // Send the contents of index.html file as the response
-    });
-  } else {
-    // If the requested URL is not "/", send a 404 Not Found response
-    res.writeHead(404);
-    res.end("404 Not Found");
+  switch (req.url) {
+    case "/":
+      serveHTMLFile("index.html", res);
+      break;
+    case "/about":
+      serveHTMLFile("about.html", res);
+      break;
+    case "/contact-me":
+      serveHTMLFile("contact-me.html", res);
+      break;
+    default:
+      serveHTMLFile("404.html", res);
+      break;
   }
 });
+
+function serveHTMLFile(filename, res) {
+  fs.readFile(path.join(__dirname, filename), (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end(`Error loading ${filename}: ${err}`);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
